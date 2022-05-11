2cf4324d1b2a8f9ad3feee6509413ffc6a6ef25c
Kamig95 <kamilgrab95@gmail.com>
2022-05-11 21:46:04 +0200

    last changes before review v2

--- utils/get_data_for_model.py
+++ utils/get_data_for_model.py
@@ -8,30 +8,28 @@ from statistics import mean
 from utils.Constants import PATH_IMAGES
 from utils.Constants import PATH_DATA
 
-#pytesseract.pytesseract.tesseract_cmd = r'C:\ProgramData\Anaconda3\envs\DataManager\Lib\site-packages'
-pytesseract.pytesseract.tesseract_cmd = r'C:\ProgramTesseract-OCR'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program files\Tesseract-OCR\tesseract.exe'
 
 
 def get_data_for_model(file_name: str):
-    im = Image.open(f'{PATH_IMAGES}\\{file_name}')
+    im = Image.open(f'{PATH_IMAGES}\{file_name}')
     im.save('ocr.png', dpi=(300, 300))
     image = cv2.imread('ocr.png')
     image = cv2.resize(image, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
     _, threshold = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
 
-    text = pytesseract.image_to_pdf_or_hocr(threshold, extension='hocr')
+    text = pytesseract.image_to_pdf_or_hocr(threshold, extension='hocr', lang='pol')
     number_of_tokens = 0
     number_of_white_spaces = 0
     scores = []
-    for i, line in enumerate(str(text).split('x_wconf')):
-        if i != 0:
+    for i_, line in enumerate(str(text).split('x_wconf')):
+        if i_ != 0:
             scores.append(int((line.split('</span>')[0]).split('\\')[0]))
         sentence = (line.split('</span')[0]).split('>')[1:]
         joined_sentence = ' '.join(sentence)
         number_of_tokens += 1
         if joined_sentence.isspace():
             number_of_white_spaces += 1
-
     return f'{file_name},{float(mean(scores) / 100)},{number_of_tokens},{number_of_white_spaces / number_of_tokens}'
 
 
